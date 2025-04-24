9486fde9dbf940d6a228a43f5e8b185012aa5d46
Abhishek-2416 <abhishekalimchandani1624@protonmail.com>
2025-04-25 04:24:49 +0530

    Shared Libraries Jenkins and Groovy Pipeline

--- 10-Shared-Libraries-Pipeline/vars/npmInstallAndBuild.groovy
+++ 10-Shared-Libraries-Pipeline/vars/npmInstallAndBuild.groovy
@@ -1,11 +1,11 @@
-def call(String APP_DIR = '10-Shared-Libraries-Pipeline'){
-    dir(APP_DIR){
-        steps{
-            sh 'npm install'
-            sh 'npm run build'
-        }
+// vars/npmInstallAndBuild.groovy
+def call(String APP_DIR = '10-Shared-Libraries-Pipeline') {
+    dir(APP_DIR) {
+        sh 'npm install'
+        sh 'npm run build'
     }
 }
+
 
 // def call(String APP_DIR = '10-Shared-Libraries-Pipeline'){
 //     dir(APP_DIR){
